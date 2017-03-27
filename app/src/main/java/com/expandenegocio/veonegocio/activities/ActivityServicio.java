0ed8b0ee9df61f99896bed3af755bf3479486dfb
jesus <jesusvillah@hotmail.com>
2017-03-27 09:58:27 +0200

    servicios y privacidad leer html , aun no esta

--- app/src/main/java/com/expandenegocio/veonegocio/activities/ActivityServicio.java
+++ app/src/main/java/com/expandenegocio/veonegocio/activities/ActivityServicio.java
@@ -3,8 +3,11 @@ package com.expandenegocio.veonegocio.activities;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -15,74 +18,87 @@ import com.expandenegocio.veonegocio.R;
  * Created by jesus on 23/03/2017.
  */
 
-public class ActivityServicio extends AppCompatActivity {
-    private ListView mLeadsList;
-    private ArrayAdapter<String> mLeadsAdapter;
+public class ActivityServicio extends AppCompatActivity implements View.OnClickListener {
+
+    WebView simpleWebView;
+    Button loadWebPage, loadFromStaticHtml;
+    String customHtml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_terminos_de_servicio);
-      //  mLeadsList = (ListView) this.findViewById(R.id.list_view_terminos_de_servicio);
-        WebView webView = new WebView(this);
-        //WebView webView= new WebView();
-        webView.loadUrl("file: ///android_assets/servicios.html");
-        webView=(WebView)findViewById(R.id.web_servicios);
-/*
-        String[] leadsNames = {
-                "Con cien cañones por banda," +
-                        "\nviento en popa a toda vela," +
-                        "\nno corta el mar, sino vuela," +
-                        "\nun velero bergantín:" +
-                        "\n\nbajel pirata que llaman" +
-                        "\npor su bravura el Temido," +
-                        "\nen todo mar conocido" +
-                        "\ndel uno al otro confín." +
-                        "\n\nLa luna en el mar riela," +
-                        "\nen la lona gime el viento," +
-                        "\ny alza en blando movimiento" +
-                        "\nolas de plata y azul;" +
-                        "\n\ny ve el capitán pirata," +
-                        "\ncantando alegre en la popa," +
-                        "\nAsia a un lado, al otro Europa" +
-                        "\ny allá a su frente Stambul. " +
-
-                        "\n\nNavega, velero mío," +
-                        "sin temor," +
-                        "\nque ni enemigo navío," +
-                        "\nni tormenta, ni bonanza," +
-                        "\ntu rumbo a torcer alcanza," +
-                        "\nni a sujetar tu valor. " +
-
-                        "\n\nVeinte presas" +
-                        "hemos hecho" +
-                        "\na despecho" +
-                        "del inglés," +
-                        "\ny han rendido" +
-                        "sus pendones" +
-                        "\ncien naciones" +
-                        "a mis pies. " +
-
-                        "\n\n¿Qué es mi barco?Mi tesoro." +
-                        "\n¿Qué es mi Dios?La libertad. " +
-                        "\n¿Mi ley? ¡La fuerza y el viento !" +
-                        "\n¿Mi única patria ? ¡La mar ! "
-
-
-        };
-
-        mLeadsAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                leadsNames);
-        mLeadsList.setAdapter(mLeadsAdapter);
-        */
-        /*
-        WebView web = new WebView ();
-web.loadUrl ( "file: ///android_asset/sample.html");
-asegurarse de que lances la vista web antes de utilizar la función LoadURL ():
-web = (WebView) findViewById (R.id.webview);
-         */
+        setContentView(R.layout.layout_servicio_privacidad);
+        loadFromStaticHtml = (Button) findViewById(R.id.privacidad);
+        loadFromStaticHtml.setOnClickListener(this);
+        loadWebPage = (Button) findViewById(R.id.servicios);
+        loadWebPage.setOnClickListener(this);
+        simpleWebView = (WebView) findViewById(R.id.simpleWebView);
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.servicios:
+
+                customHtml = "<html><body><h1>SERVICIOS</h1>" +
+                        "<h1>Titulo  1</h1><h2>Titulo 2</h2><h3>JESUS</h3>" +
+                        "<h3>  HTML En Web view</h3>" +
+                        "<p>Esto solo es una prueba linea 01</p>" +
+                        "<p>Esto solo es una prueba linea 02</p>" +
+                        "<p>Esto solo es una prueba linea 03</p>" +
+                        "<p>Esto solo es una prueba linea 04</p>" +
+                        "<p>Esto solo es una prueba linea 05</p>" +
+                        "<p>Esto solo es una prueba linea 06</p>" +
+                        "<p>Esto solo es una prueba linea 07</p>" +
+                        "<p>Esto solo es una prueba linea 08</p>" +
+                        "<p>Esto solo es una prueba linea 09</p>" +
+                        "<p>Esto solo es una prueba linea 10</p>" +
+
+                        "</body></html>";
+
+                simpleWebView.loadData(customHtml, "text/html", "UTF-8");
+                break;
+            case R.id.privacidad:
+                customHtml = "<html><body><h1>POLITICA PRIVACIDAD</h1>" +
+                        "<h1>Titulo 1</h1><h2>Titulo 2</h2><h3>JESUS</h3>" +
+
+                        "<p>Política de Privacidad Última modificación: 1 de marzo de"
+                        + "2017 (ver versiones archivadas) (Los ejemplos con hiperenlaces están"
+                        + "disponibles al final de este documento). Existen muchas formas diferentes de"
+                        + "utilizar nuestros servicios para buscar y compartir información, ponerte en"
+                        + "contacto con otros usuarios o crear contenido nuevo. Cuando compartes"
+                        + "información con nosotros, por ejemplo, al crear una cuenta de Google, podemos"
+                        + "mejorar esos servicios para mostrarte anuncios y resultados de búsqueda más"
+                        + "relevantes, ayudarte a conectar con personas o compartir contenido con otros"
+                        + "usuarios de forma más fácil y rápida. Ya que eres usuario de nuestros"
+                        + "servicios, queremos que entiendas cómo usamos la información y lo que puedes"
+                        + "hacer para proteger tu privacidad. La presente Política de privacidad describe:"
+                        + "Qué datos recogemos y los fines para los que llevamos a cabo su recogida Cómo"
+                        + "utilizamos esos datos Las opciones que ofrecemos, incluyendo cómo acceder a los"
+                        + "datos y actualizarlos. Aunque hemos intentado proporcionar una descripción lo"
+                        + "más sencilla posible, si no estás familiarizado con términos clave tales como"
+                        + "«cookies», «dirección IP», «contadores de visitas» y «navegador», consulta esta"
+                        + "página para informarte de su significado antes de continuar. Tu privacidad es"
+                        + "importante para Google, por lo que con independencia de que seas un usuario"
+                        + "nuevo o un usuario avanzado, te recomendamos que leas nuestra política y te"
+                        + "pongas en contacto con nosotros si tienes cualquier duda. Datos recogidos por"
+                        + "Google Recopilamos información para ofrecer mejores servicios a todos nuestros</p>" +
+
+
+                        "</body></html>";
+                simpleWebView.loadData(customHtml, "text/html", "UTF-8");
+                break;
+        }
+    }
+
+   /* private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }*/
+
+
 }
--- app/src/main/java/com/expandenegocio/veonegocio/activities/MainActivity.java
+++ app/src/main/java/com/expandenegocio/veonegocio/activities/MainActivity.java
@@ -27,17 +27,12 @@ import java.util.TreeMap;
 
 public class MainActivity extends ActionBarActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
     private SliderLayout mDemoSlider;
-    private TextView textoEnlace1;
-    private TextView textoEnlace2;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
         mDemoSlider = (SliderLayout) findViewById(R.id.slider);
@@ -50,21 +45,14 @@ public class MainActivity extends ActionBarActivity implements BaseSliderView.On
 
         for (String name : fotos_presentacion.keySet()) {
             TextSliderView textSliderView = new TextSliderView(MainActivity.this);
-            // initialize a SliderLayout
-/*
-            textSliderView
-                    .description(name)
-                    .image(fotos_presentacion.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit)
-                    .setOnSliderClickListener(MainActivity.this);
-*/
+
             textSliderView
 
                     .image(fotos_presentacion.get(name))
                     .setScaleType(BaseSliderView.ScaleType.Fit)
                     .setOnSliderClickListener(MainActivity.this);
 
-            //add your extra information
+
             textSliderView.bundle(new Bundle());
             textSliderView.getBundle()
                     .putString("extra", name);
@@ -77,16 +65,7 @@ public class MainActivity extends ActionBarActivity implements BaseSliderView.On
         mDemoSlider.setDuration(4000);
         mDemoSlider.addOnPageChangeListener(MainActivity.this);
 
-        /*
-        ListView l = (ListView) findViewById(R.id.transformes);
-        l.setAdapter(new TransformerAdapter(this));
-        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView parent, View view, int position, long id) {
-                mDemoSlider.setPresetTransformer(((TextView) view).getText().toString());
-            }
-        });
-        */
+
     }
 
 
@@ -99,7 +78,7 @@ public class MainActivity extends ActionBarActivity implements BaseSliderView.On
 
     @Override
     public void onSliderClick(BaseSliderView slider) {
-       // Toast.makeText(this, slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -116,91 +95,14 @@ public class MainActivity extends ActionBarActivity implements BaseSliderView.On
     }
 
 
-
     public void irAEnlaceTerminosDeServicio(View view) {
-        Intent intent=new Intent("AcivityServicio");
+        Intent intent = new Intent("AcivityServicio");
         startActivity(intent);
     }
 
-    public void irAEnlacePoliticaDePrivacidad(View view) {
-        Intent intent=new Intent("AcivityPrivacidad");
-        startActivity(intent);
+    public void haceralgo(View view) {
+        //
     }
 
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
-/*
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
-
-    public void comprobar(View view) {
-
-        EditText usuario = (EditText) this.findViewById(R.id.edit_nombre_usuario);
-        EditText contrasena = (EditText) this.findViewById(R.id.edit_contraseña);
-       // Toast.makeText(this, usuario.getText(), Toast.LENGTH_LONG).show();
-       // Toast.makeText(this, contrasena.getText(), Toast.LENGTH_LONG).show();
-        TextView recoger1 = (TextView) this.findViewById(R.id.respuesta1);
-        recoger1.setText( usuario.getText());
-
-        TextView recoger2 = (TextView) this.findViewById(R.id.respuesta2);
-        recoger1.setText( contrasena.getText());
-
-
-
-        //  comprobar si usuario y contraseña estan en base de datos
-        // si existe usuario  hacer algo ,nueva activity
-        // si no exixte  lanzar activity de registro
-
-
-
-
-    }
-
-    public void lanzarPresentacion(View view) {
-        Intent intent=new Intent("AcivityPresentacion");
-        startActivity(intent);
-    }
-
-    public void lanzarSinConexion(View view) {
-        Intent intent=new Intent("AcivitySinConexion");
-        startActivity(intent);
-    }
-
-    public void lanzarRegistro(View view) {
-        Intent intent=new Intent("AcivityRegistro");
-        startActivity(intent);
-
-    }
-
-    public void resolverBotonPresentacion(View view) {
-    }
-}
-*/
