6ed46fd17121f7828c118d14cc88bffbaf83f1cf
jesus <jesusvillah@hotmail.com>
2017-05-31 20:37:45 +0200

    falta rellenar arraylist de subsectoes seleccionados en altas de usuarios

--- app/src/main/java/com/expandenegocio/veonegocio/activities/MainActivity.java
+++ app/src/main/java/com/expandenegocio/veonegocio/activities/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends ActionBarActivity implements BaseSliderView.On
 
         MDB = new DbHelper(getApplicationContext());
         // borra basedatos
-          getApplicationContext().deleteDatabase("VeoNegocio.db");
+          //getApplicationContext().deleteDatabase("VeoNegocio.db");
         // borra datos
         MDB.borrarProvincias();
         MDB.borrarMunicipios();
